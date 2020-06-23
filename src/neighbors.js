c0deace1a809b2e854e2df7625f404e88fb8e2af
lightfire224 <elias.taveras@columbia.edu>
2020-06-23 15:49:47 -0400

    edges working!!!

--- src/neighbors.js
+++ src/neighbors.js
@@ -1,13 +1,13 @@
-
 function neighbors(node){
     directions = [ [1,0], [0,1], [-1,0], [0, -1]]
     result= []
     
     for ( direction in directions){
         neighbor = [node[0] + directions[direction][0], node[1] + directions[direction][1]]        
+        console.log(neighbor)
 
-        if allNodes.includes(neighbor){
-
+        if (0 <= neighbor[0] < 20 && 0 <= neighbor[1] < 20){
+            result.push(neighbor)
         }
     }
     return result
