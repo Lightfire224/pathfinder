2eb0672611f8a0a03a82d3cc1428f443fbb1161b
lightfire224 <elias.taveras@columbia.edu>
2020-06-24 00:14:30 -0400

    dfs console

--- src/neighbors.js
+++ src/neighbors.js
@@ -1,17 +1,65 @@
-
-function neighbors(node){
+function neighbors(grid, row, col){
     directions = [ [1,0], [0,1], [-1,0], [0, -1]]
     result= []
     
-    for ( direction in directions){
-        neighbor = [node[0] + directions[direction][0], node[1] + directions[direction][1]]        
-        console.log(neighbor)
-
-        if (0 <= neighbor[0] < 20 && 0 <= neighbor[1] < 20){
-            result.push(neighbor)
+    directions.forEach((offset) => {
+        const neighbor = [
+            row + offset[0], 
+            col + offset[1]
+        ];
+        if (neighbor[0] >= 0 
+            && neighbor[0] < grid.length 
+            && neighbor[1] >= 0 
+            && neighbor[1] < grid[0].length) {
+            result.push(neighbor);
         }
-    }
+    });
+
+    // console.log("The edge nodes of %j are %j", grid[row][col], [row, col], result)
     return result
 }
 
-console.log("The edges of [0,0] are: ", neighbors([0,0]))
+const grid = [
+    /* 0, 1, 2,  3,  4  - columns innner array*/
+    [11, 12, 13, 14, 15], // 0 - rows - outter array
+    [21, 22, 23, 24, 25], // 1
+    [31, 32, 33, 34, 35], // 2
+]
+
+function dfs(grid, row, col, visited) {
+    // const isVisited = visited.some(coord => {
+    //     return coord[0] === row && coord[1] === col
+    // })
+    if (visited.has([row, col].join(","))) {
+        return
+    }
+    console.log("Visting", row, col, "with value", grid[row][col])
+    visited.add([row,col].join(","))
+    const nodeNeighbors = neighbors(grid, row, col)
+    for (nodeIdx in nodeNeighbors){
+        const node = nodeNeighbors[nodeIdx]
+        dfs(grid, node[0], node[1], visited)
+    }
+    // console.log(visited)
+}
+
+/// Tests
+function testDFS() {
+    dfs(grid, 0, 0, new Set());
+    console.log('solving next..')
+    dfs(grid, 0, 1, new Set());
+}
+
+function testNeighbor() {
+    // grid[/* row index */][/* columns index */]
+    console.log("Rows: ", grid.length, "Columns: ", grid[0].length)
+
+    neighbors(grid, 1, 2)
+    console.log('some edges')
+    neighbors(grid, 0, 0)
+    neighbors(grid, 2, 4)
+    neighbors(grid, 2, 0)
+    console.log(neighbors(grid, 1, 2).map(coord => grid[coord[0]][coord[1]]))
+}
+
+testDFS()
