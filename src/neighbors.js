dcb076d82f8df9bbcee93531f94cf937d18ec48c
lightfire224 <elias.taveras@columbia.edu>
2020-06-25 21:23:44 -0400

    bfs working in console

--- src/neighbors.js
+++ src/neighbors.js
@@ -1,15 +1,15 @@
-function neighbors(grid, row, col){
-    directions = [ [1,0], [0,1], [-1,0], [0, -1]]
-    result= []
-    
+function neighbors(grid, row, col) {
+    directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    result = []
+
     directions.forEach((offset) => {
         const neighbor = [
-            row + offset[0], 
+            row + offset[0],
             col + offset[1]
         ];
-        if (neighbor[0] >= 0 
-            && neighbor[0] < grid.length 
-            && neighbor[1] >= 0 
+        if (neighbor[0] >= 0
+            && neighbor[0] < grid.length
+            && neighbor[1] >= 0
             && neighbor[1] < grid[0].length) {
             result.push(neighbor);
         }
@@ -20,8 +20,8 @@ function neighbors(grid, row, col){
 }
 
 const grid = [
-    /* 0, 1, 2,  3,  4  - columns innner array*/
-    [11, 12, 13, 14, 15], // 0 - rows - outter array
+    /* 0, 1, 2,  3,  4  - columns innner array (second number)*/
+    [11, 12, 13, 14, 15], // 0 - rows- outter array (first number)
     [21, 22, 23, 24, 25], // 1
     [31, 32, 33, 34, 35], // 2
 ]
@@ -34,21 +34,55 @@ function dfs(grid, row, col, visited) {
         return
     }
     console.log("Visting", row, col, "with value", grid[row][col])
-    visited.add([row,col].join(","))
+    visited.add([row, col].join(","))
     const nodeNeighbors = neighbors(grid, row, col)
-    for (nodeIdx in nodeNeighbors){
-        const node = nodeNeighbors[nodeIdx]
+    for (node of nodeNeighbors) {
         dfs(grid, node[0], node[1], visited)
     }
+    // for (nodeIdx in nodeNeighbors){
+    //     const node = nodeNeighbors[nodeIdx]
+    //     dfs(grid, node[0], node[1], visited)
+    // }
     // console.log(visited)
 }
 
-/// Tests
-function testDFS() {
-    dfs(grid, 0, 0, new Set());
-    console.log('solving next..')
-    dfs(grid, 0, 1, new Set());
+// I have to pass in the starting row/column to the queue
+function bfs(grid, row, col, visited) {
+    console.log(grid, row, col, visited)
+    console.log(visited)
+    const queue = [[row, col]]
+
+    while (queue.length > 0) {
+        const current = queue.shift()
+        // console.log(current[0], current[1])
+        console.log("Visting", current, "with value", grid[current[0]][current[1]])
+
+        visited.add([current[0], current[1]].join(","))
+        // console.log(visited)
+
+        const nodeNeighbors = neighbors(grid, current[0], current[1])
+        for ([r, c] of nodeNeighbors) {
+            if (!visited.has([r, c].join(","))) {
+                queue.push([r, c])
+            }
+        }
+    }
+
 }
+
+
+function testBFS() {
+    bfs(grid, 0, 0, new Set())
+}
+testBFS()
+
+
+/// Tests
+// function testDFS() {
+//     dfs(grid, 0, 0, new Set());
+//     console.log('solving next..')
+//     dfs(grid, 0, 1, new Set());
+// }
 
 function testNeighbor() {
     // grid[/* row index */][/* columns index */]
@@ -62,4 +96,4 @@ function testNeighbor() {
     console.log(neighbors(grid, 1, 2).map(coord => grid[coord[0]][coord[1]]))
 }
 
-testDFS()
+// testDFS()
