872b90e92afe724fbaf57886f38575cc2a82a638
lightfire224 <elias.taveras@columbia.edu>
2020-07-08 23:21:44 -0400

    new resfresh

--- src/GridContainer.js
+++ src/GridContainer.js
@@ -12,8 +12,9 @@ export default class GridContainer extends React.Component {
         startPosition: null,
         endPosition: null,
         wallPosition: [],
+        wallRefresh: null
     }
-    
+
     resetGrid = () => {
         this.setState({
             visitedNumbers: new Set(),
@@ -23,167 +24,171 @@ export default class GridContainer extends React.Component {
             endPosition: null,
             wallPosition: [],
         })
+        this.createNodes()
     }
 
     componentDidMount() {
         this.createNodes()
     }
 
-        createNodes() {
-            const grid = []
-            for (let row = 0; row < 10; row++) {
-                const rowData = []
-                for (let col = 0; col < 10; col++) {
-                    let tensPlace = row * 10
-                    const columnNumber = tensPlace + col
-                    rowData.push({ value: columnNumber, weight: 0 })
-                }
-                grid.push(rowData)
+    createNodes() {
+        const grid = []
+        for (let row = 0; row < 10; row++) {
+            const rowData = []
+            for (let col = 0; col < 10; col++) {
+                let tensPlace = row * 10
+                const columnNumber = tensPlace + col
+                rowData.push({ value: columnNumber, weight: 0 })
             }
-            this.setState({ grid })
+            grid.push(rowData)
+        }
+        this.setState({ grid })
+    }
+
+
+    updateVisited = (visited) => {
+        this.setState({
+            visitedNumbers: new Set([...Array.from(visited)]),
+        })
+    }
+
+
+    updateNeighbors = (neighbors) => {
+        this.setState({
+            neighborList: [...neighbors, neighbors]
+        })
+    }
+
+
+    getCellStyle = (rowIdx, colIdx) => {
+        if (this.state.startPosition) {
+            if (this.state.startPosition[0] === rowIdx && this.state.startPosition[1] === colIdx) {
+                return { backgroundColor: "lawngreen" }
+            }
         }
 
-
-        updateVisited = (visited) => {
-            this.setState({
-                visitedNumbers: new Set([...Array.from(visited)]),
-            })
+        if (this.state.endPosition) {
+            if (this.state.endPosition[0] === rowIdx && this.state.endPosition[1] === colIdx) {
+                return { backgroundColor: "coral" }
+            }
         }
 
-
-        updateNeighbors = (neighbors) => {
-            this.setState({
-                neighborList: [...neighbors, neighbors]
-            })
+        if (this.state.wallPosition) {
+            for (const wall of this.state.wallPosition) {
+                if (wall[0] === rowIdx && wall[1] === colIdx) {
+                    return { backgroundColor: "rgba(0, 0, 0, 0.411)" }
+                }
+            }
         }
 
+        const isVisited = this.state.visitedNumbers.has([rowIdx, colIdx].join(","))
 
-        getCellStyle = (rowIdx, colIdx) => {
-            if (this.state.startPosition) {
-                if (this.state.startPosition[0] === rowIdx && this.state.startPosition[1] === colIdx) {
-                    return { backgroundColor: "lawngreen" }
-                }
+        for (const neighbor of this.state.neighborList) {
+            let neighborRowIdx = neighbor[0]
+            let neighborColIdx = neighbor[1]
+            if (neighborRowIdx === rowIdx && neighborColIdx === colIdx) {
+                return { backgroundColor: "crimson" }
             }
-
-            if (this.state.endPosition) {
-                if (this.state.endPosition[0] === rowIdx && this.state.endPosition[1] === colIdx) {
-                    return { backgroundColor: "coral" }
-                }
-            }
-
-            if (this.state.wallPosition) {
-                for (const wall of this.state.wallPosition) {
-                    if (wall[0] === rowIdx && wall[1] === colIdx) {
-                        return { backgroundColor: "rgba(0, 0, 0, 0.411)" }
-                    }
-                }
-            }
-
-            const isVisited = this.state.visitedNumbers.has([rowIdx, colIdx].join(","))
-
-            for (const neighbor of this.state.neighborList) {
-                let neighborRowIdx = neighbor[0]
-                let neighborColIdx = neighbor[1]
-                if (neighborRowIdx === rowIdx && neighborColIdx === colIdx) {
-                    return { backgroundColor: "crimson" }
-                }
-            }
-
-            return {
-                backgroundColor: isVisited ? "DeepPink" : "gold",
-                animation: isVisited ? "cellpulse 0.5s linear" : "none"
-            };
         }
 
+        return {
+            backgroundColor: isVisited ? "DeepPink" : "gold",
+            animation: isVisited ? "cellpulse 0.5s linear" : "none"
+        };
+    }
 
-        //the console.log on line 69 slows the function down, i need to make it wait more
 
-        render() {
-            return (
-                <div>
-                    <div className="grid-container">
-            <button onClick={this.resetGrid} > Reset Grid</button>
-                        <button onClick={this.editStartNode}>Place Start Node</button>
-                        <button onClick={this.editEndNode}> Place End Node</button>
-                        <button onClick={this.editWall}> Place Wall Node</button>
-                        <button onClick={this.editWeight}> Place Weight on Node</button>
-                        {this.state.grid.map((row, rowIdx) => {
-                            return (
-                                <div key={rowIdx}>
-                                    {row.map(({ value,weight }, colIdx) => {
-                                        return (
-                                            <div
-                                                onClick={() => this.setNode(rowIdx, colIdx)}
-                                                style={this.getCellStyle(rowIdx, colIdx)}
-                                                className="cell"
-                                                key={value}>{this.state.grid[rowIdx][colIdx].weight}
-                                            </div>)
-                                    })}
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div id="algo-input-container">
-                        <Algorithms
-                            grid={this.state.grid}
-                            startPosition={this.state.startPosition}
-                            endPosition={this.state.endPosition}
-                            isVisited={this.updateVisited}
-                            updateNeighbors={this.updateNeighbors}
-                            wallPosition={this.state.wallPosition}
-                        />
-                        <InputContainer
-                         reRender={this.fetchData}
-                         />
-                    </div>
+    //the console.log on line 69 slows the function down, i need to make it wait more
+
+    render() {
+        return (
+            <div>
+                <div className="grid-container">
+                    <button onClick={this.resetGrid} > Reset Grid</button>
+                    <button onClick={this.editStartNode}>Place Start Node</button>
+                    <button onClick={this.editEndNode}> Place End Node</button>
+                    <button onClick={this.editWall}> Place Wall Node</button>
+                    <button onClick={this.editWeight}> Place Weight on Node</button>
+                    {this.state.grid.map((row, rowIdx) => {
+                        return (
+                            <div key={rowIdx}>
+                                {row.map(({ value, weight }, colIdx) => {
+                                    return (
+                                        <div
+                                            onClick={() => this.setNode(rowIdx, colIdx)}
+                                            style={this.getCellStyle(rowIdx, colIdx)}
+                                            className="cell"
+                                            key={value}>{this.state.grid[rowIdx][colIdx].weight}
+                                        </div>)
+                                })}
+                            </div>
+                        )
+                    })}
                 </div>
+                <div id="algo-input-container">
+                    <Algorithms
+                        grid={this.state.grid}
+                        startPosition={this.state.startPosition}
+                        endPosition={this.state.endPosition}
+                        isVisited={this.updateVisited}
+                        updateNeighbors={this.updateNeighbors}
+                        wallPosition={this.state.wallPosition}
+                    />
+                    <InputContainer
+                        reRender={this.fetchData}
+                    />
+                </div>
+            </div>
 
-            )
-        }
+        )
+    }
 
-        editStartNode = () => {
+    editStartNode = () => {
+        this.setState({
+            editMode: 0,
+        })
+    }
+
+    editEndNode = () => {
+        this.setState({
+            editMode: 1,
+        })
+    }
+
+    editWall = () => {
+        this.setState({
+            editMode: 2,
+        })
+    }
+
+    editWeight = () => {
+        this.setState({
+            editMode: 3
+        })
+    }
+
+    setNode = (rowIdx, colIdx) => {
+        if (this.state.editMode === 0) {
             this.setState({
-                editMode: 0,
+                startPosition: [rowIdx, colIdx]
             })
         }
-
-        editEndNode = () => {
+        if (this.state.editMode === 1) {
             this.setState({
-                editMode: 1,
+                endPosition: [rowIdx, colIdx]
             })
         }
-
-        editWall = () => {
+        if (this.state.editMode === 2) {
             this.setState({
-                editMode: 2,
+                wallPosition: [...this.state.wallPosition, [rowIdx, colIdx]]
             })
         }
-
-        editWeight = () => {
+        if (this.state.editMode === 3) {
+            console.log(this.state.grid[rowIdx][colIdx].weight += 10)
             this.setState({
-                editMode: 3
+                wallRefresh: 0
             })
         }
-
-        setNode = (rowIdx, colIdx) => {
-            if (this.state.editMode === 0) {
-                this.setState({
-                    startPosition: [rowIdx, colIdx]
-                })
-            }
-            if (this.state.editMode === 1) {
-                this.setState({
-                    endPosition: [rowIdx, colIdx]
-                })
-            }
-            if (this.state.editMode === 2) {
-                this.setState({
-                    wallPosition: [...this.state.wallPosition, [rowIdx, colIdx]]
-                })
-            }
-            if(this.state.editMode === 3){
-                console.log(this.state.grid[rowIdx][colIdx].weight+=10)
-            }
-        }
+    }
 
 }
